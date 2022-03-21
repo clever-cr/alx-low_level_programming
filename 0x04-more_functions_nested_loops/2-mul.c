4fcd30d5402a37d924a4631a164f1fa8622e78b5
root <cleverumuhuza@gmail.com>
2022-03-21 13:45:34 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - add first two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: value
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
