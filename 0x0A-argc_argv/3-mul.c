5ddd4a17bfc0d7b2e8d23bc6d788537c4f8d8b23
clever-cr <cleverumuhuza@gmail.com>
2022-04-06 15:29:13 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - multiple of two numbers
+*@argc: Number of command line argument 
+*@argv: Array name
+ */
+int main(int argc, char *argv[])
+{
+int i, j;
+
+if (argc == 1 || argc == 2)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+j = 1;
+
+for (i = 1; i < 3; i++)
+j *= atoi(argv[i]);
+
+printf("%d\n", j);
+}
+return (0);
+}
+
+
