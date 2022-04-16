66a2158548244037535e4b02f539dd8a76cf254f
clever-cr <cleverumuhuza@gmail.com>
2022-04-16 02:59:40 -0700

    prit numbers

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,25 +1,23 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 
 /**
- * sum_them_all - function that returns the sum of all its parameters
- * @n: const unsigned int
- * Return: 0 if n == 0 else sum
+ * sum_them_all - calculates the sum of all its parameters
+ * @n: number of arguments passed to the function
+ *
+ * Return: the resulting sum
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
-	int sum = 0, j;
-	va_list args;
+unsigned int i;
+int sum = 0;
+va_list list;
 
-	va_start(args, n);
+va_start(list, n);
 
-	if (n == 0)
-		return (0);
-	for (i = 0; i < n; i++)
-	{
-		j = va_arg(args, int);
-		sum = sum + j;
-	}
-	return (sum);
+for (i = 0; i < n; i++)
+sum += va_arg(list, int);
+
+va_end(list);
+
+return (sum);
 }
