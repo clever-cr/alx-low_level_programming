eed12d4d286f85b437837825f57c3b6331870531
clever-cr <cleverumuhuza@gmail.com>
2022-04-25 07:54:16 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,12 +18,12 @@ list_t *add_node(list_t **head, const char *str)
 
     new = malloc(sizeof(list_t));
     if (!new)
-    return (NULL);
+return (NULL);
 
-    new->str = strdup(str);
-    new->len = len;
-    new->next = (*head);
-    (*head) = new;
+new->str = strdup(str);
+new->len = len;
+new->next = (*head);
+(*head) = new;
 
-    return (*head); 
+return (*head); 
 }
