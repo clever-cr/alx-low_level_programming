1c7db20eb9a3d9b619152326c6faa2121108acf7
clever-cr <cleverumuhuza@gmail.com>
2022-05-03 17:09:07 -0700

    create file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,44 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
- *
- * Return: the number of letters printed, or 0 if it failed
+ * read_textfile - function that reads a text file and prints it to the
+ * POSIX standard output
+ * @filename: file to read and write
+ * @letters: the number of letters it should read and print
+ * Return: letters printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int s, t;
-	char *buf;
+	ssize_t nletters;
+	int file;
+	char *text;
 
 	if (!filename)
 		return (0);
-
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	text = malloc(sizeof(char) * letters + 1);
+	if (text == NULL)
 		return (0);
-
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-
-	s = read(fd, buf, letters);
-	if (s < 0)
+	file = open(filename, O_RDONLY);
+	if (file == -1)
 	{
-		free(buf);
+		free(text);
 		return (0);
 	}
-	buf[s] = '\0';
-
-	close(fd);
-
-	t = write(STDOUT_FILENO, buf, s);
-	if (t < 0)
+	nletters = read(file, text, sizeof(char) * letters);
+	if (nletters == -1)
 	{
-		free(buf);
+		free(text);
+		close(file);
 		return (0);
 	}
-
-	free(buf);
-	return (t);
+	nletters = write(STDOUT_FILENO, text, nletters);
+	if (nletters == -1)
+	{
+		free(text);
+		close(file);
+		return (0);
+	}
+	free(text);
+	close(file);
+	return (nletters);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,34 +2,46 @@
 #include "main.h"
 
 /**
- * create_file - creates a file and fills it with text
- * @filename: name of the file to create
- * @text_content: text to write in the file
- *
+ * _strlen - length of a string
+ * @s: input char
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: the name of file to create
+ * @text_content: info to write into the file
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, t, s = 0;
+	ssize_t nletters;
+	int file;
 
 	if (!filename)
+		return (1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd < 0)
-		return (-1);
-
 	if (text_content)
 	{
-		while (text_content[s])
-			s++;
-		t = write(fd, text_content, s);
-		if (t != s)
+		nletters = write(file, text_content, _strlen(text_content));
+		if (nletters == -1)
+		{
+			close(file);
 			return (-1);
+		}
 	}
-
-	close(fd);
-
+	close(file);
 	return (1);
 }
-
